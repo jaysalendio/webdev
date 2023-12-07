6cde743525b9eb6ad6d45715313dd6710287801d
jaysa <22104713@usc.edu.ph>
2023-12-07 18:42:12 +0800

    add file

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult input()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddUserToDatabase(FormCollection fc)
+        {
+            String firstName = fc["firstname"];
+            String lastName = fc["lastname"];
+            String email = fc["email"];
+            String diko = fc["password"];
+
+            user use = new user();
+            use.firstname = firstName;
+            use.lastname = lastName;
+            use.email = email;
+            use.password = diko;
+            use.roleID = 1;
+
+            friendsEntities fe = new friendsEntities();
+            fe.users.Add(use);
+            fe.SaveChanges();
+
+            //insert the code that will save these information to the DB
+
+            return RedirectToAction("input");
+        }
+
+        public ActionResult UserUpdate()
+        {
+            friendsEntities rdbe = new friendsEntities();
+            user u = (from a in rdbe.users
+                      where a.userId == 1
+                      select a).FirstOrDefault();
+            u.firstname = "Jaysa";
+            u.lastname = "Rivas";
+            u.email = "jaysa@gmail.com";
+            u.password = "jaysa123";
+            u.roleID = 1;
+            rdbe.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult UserDelete()
+        {
+            friendsEntities rdbe = new friendsEntities();
+            user u = (from a in rdbe.users
+                      where a.userId == 3
+                      select a).FirstOrDefault();
+            rdbe.users.Remove(u);
+            rdbe.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult ShowUser()
+        {
+            friendsEntities fe = new friendsEntities();
+            var userList = (from a in fe.users
+                            select a).ToList();
+
+            ViewData["UserList"] = userList;
+            return View();
+        }
+    }
+}
